e096b4ebd615a0ee64a60e686e275befda6ca80c
Viswazgummadi <viswazgummadi@gmail.com>
2024-12-09 12:25:58 +0000

    done

--- STL/bot.cc
+++ STL/bot.cc
@@ -2,18 +2,15 @@
 using namespace std;
 
 int main() {
-    set<int> st;
+    vector<int> vec(10, 0);
 
-    st.insert(10);
-    st.insert(11);
-    st.insert(12);
-    st.insert(13);
-    st.insert(11);
-
-    for(auto i : st)
+    for(int i:vec)
     {
         cout << i << " ";
     }
+
     cout << endl;
+
+    cout << vec.at(11);
     return 0;
 }
